69e747ce7c68700d4eba8a10db5762b488224724
Nazim Uddin <umnazim@gmail.com>
2020-09-06 20:13:03 -0700

    Contexts folder and uniqid package added

---
+++ book-lists/src/contexts/BookContext.js
@@ -0,0 +1,35 @@
+import React, { createContext, useState } from "react";
+import uniqid from "uniqid";
+
+export const BookContext = createContext();
+
+const BookContextProvider = ({ children }) => {
+  const [books, setBooks] = useState([
+    {
+      id: 1,
+      title: "All The King's Men",
+      author: "Robert Penn Warren",
+    },
+    {
+      id: 2,
+      title: "On The Road",
+      author: "Jack Kerouac",
+    },
+  ]);
+
+  const addBook = (book) => {
+    setBooks([...books, { ...book, id: uniqid() }]);
+  };
+
+  const deleteBook = (id) => {
+    setBooks(books.filter((book) => book.id !== id));
+  };
+
+  return (
+    <BookContext.Provider value={{ books, addBook, deleteBook }}>
+      {children}
+    </BookContext.Provider>
+  );
+};
+
+export default BookContextProvider;
