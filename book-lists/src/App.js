9337726e371089b72ae68c1133f181f93280dd0c
Nazim Uddin <umnazim@gmail.com>
2020-09-06 20:36:57 -0700

    components, Book context and routes created

--- book-lists/src/App.js
+++ book-lists/src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React from "react";
+import BookContextProvider from "./contexts/BookContext";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 import Footer from "./components/Footer";
+import "./App.css";
 
-
-function App() {
+const App = () => {
   return (
-    <div className="container">
-      <h1>Favorite Book lists</h1>
-      <Footer/>
-    </div>
+    <BookContextProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+        <Footer />
+      </BrowserRouter>
+    </BookContextProvider>
   );
-}
+};
 
 export default App;
---
+++ book-lists/src/components/Dashboard.jsx
@@ -0,0 +1,15 @@
+
+import React, { Fragment } from 'react';
+import BookList from './BookList';
+import BookForm from './BookForm';
+
+const Dashboard = () => {
+  return (
+    <Fragment>
+      <BookList />
+      <BookForm />
+    </Fragment>
+  );
+};
+
+export default Dashboard;
